f1c365fb80959d32cd7c2e9fd0c996267fb461b9
gaspard souvay <gaspard.souvay@epita.fr>
2023-10-11 13:18:23 +0200

    fixed mem leak

--- new-neural-network/training/training.c
+++ new-neural-network/training/training.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
+
+#define LEARNING_RATE 1
 
 double* target_array(int target, size_t len)
 {
@@ -10,7 +13,22 @@ double* target_array(int target, size_t len)
     double* res = calloc(len, sizeof(double));
     for (size_t i = 0; i < len; i++)
     {
-	res[i] = (i == target) ? 0.97 : 0.03;
+	res[i] = (i == target) ? 1 : 0;
+    }
+    return res;
+}
+
+double get_cost(double* output, double* target, size_t i)
+{
+    return LEARNING_RATE * pow((target[i] - output[i]),2);
+}
+
+double get_total_cost(double* output, double* target, size_t len)
+{
+    double res = 0;
+    for(size_t i = 0; i < len; i++)
+    {
+	res += get_cost(output, target, i);
     }
     return res;
 }
@@ -18,7 +36,67 @@ double* target_array(int target, size_t len)
 void gradient_descent(double* input, double* target, Mlp* n)
 {
     //updates weights of network using gradient descent
-    
+    size_t i, w, h;
+    double*** gradient; //weights
+    double** gradient_b; //biases
+    double** activation;
+
+    //allocate memory for gradient
+    gradient = calloc(n->count, sizeof(double**));
+    gradient_b = calloc(n->count, sizeof(double*));
+
+    //allocate memory for each layer's gradient
+    for(i = 0; i < n->count; i++)
+    {
+	gradient[i] = calloc(n->layers[i].w, sizeof(double*));
+	gradient_b[i] = calloc(n->layers[i].w, sizeof(double));
+	//allocate memory for each neuron's weights
+	for(w = 0; w < n->layers[i].w; w++)
+	{
+	    gradient[i][w] = calloc(n->layers[i].h, sizeof(double));
+	}
+    }
+
+    //get activation of mlp with given input
+    activation = get_activation(input, n);
+
+    //compute gradient
+
+    //apply gradient to weights
+    for(i = 0; i < n->count; i++)
+    {
+	for(w = 0; w < n->layers[i].w; w++)
+	{
+	    for(h = 0; h < n->layers[i].h; h++)
+	    {
+		n->layers[i].weights[w][h] += gradient[i][w][h];
+	    }
+	}
+    }
+
+    //apply gradient to biases
+    for(i = 0; i < n->count; i++)
+    {
+	for(w = 0; w < n->layers[i].w; w++)
+	{
+	    n->layers[i].biases[w] += gradient_b[i][w];
+	}
+    }
+
+    //free gradients and activation
+    for(i = 0; i < n->count; i++)
+    {
+	for(w = 0; w < n->layers[i].w; w++)
+	{
+	    free(gradient[i][w]);
+	}
+	free(gradient[i]);
+	free(gradient_b[i]);
+	free(activation[i]);
+    }
+    free(gradient);
+    free(gradient_b);
+    free(activation);
 }
 
 void train(double* input, int target, Mlp* network, size_t len)
--- new-neural-network/training/training.h
+++ new-neural-network/training/training.h
@@ -1,6 +1,8 @@
 #ifndef TRAINING_H
 #define TRAINING_H
 
+double get_total_cost(double* output, double* target, size_t len);
+
 void train(double* input, int target, Mlp* network, size_t len);
 
 #endif 
